fb7dbd684f63dd8ddc3719eb80c916875f1577da
Moonshot <>
2022-11-07 01:54:04 +0100

    a scene to give wallet related info messages

---
+++ src/scenes/Wallet.ts
@@ -0,0 +1,47 @@
+import Phaser from "phaser";
+import * as SceneFactory from '../scripts/SceneFactory';
+export default class Wallet extends Phaser.Scene {
+    
+    private image!: Phaser.GameObjects.Image;
+    
+    constructor() {
+        super('wallet');
+     }
+
+    preload() {
+        this.load.image('bg', 'assets/storyx.webp');
+
+        SceneFactory.preload(this);
+    }
+
+    create() {
+        const { width, height } = this.scale;
+
+        if(globalThis.noWallet) {
+            this.add.bitmapText(width * 0.5, height / 2 + 160, 'press_start', 'It seems you dont have a wallet (yet)', 22)
+                .setTint(0xffffff)
+                .setOrigin(0.5);
+        }
+        else if(globalThis.moonshotBalance == 0 && globalThis.ra8bitBalance == 0 ) {
+            this.add.bitmapText(width * 0.5, height / 2 + 160, 'press_start', 'You are allowed access Level 1', 22)
+                .setTint(0xffffff)
+                .setOrigin(0.5);
+        }
+
+        this.add.bitmapText(width * 0.5, height / 2 + 210, 'press_start', 'Unlimited play only for Moonshot and Ra8bits holders', 22)
+            .setTint(0xffffff)
+            .setOrigin(0.5); 
+
+        this.image = this.add.image(width / 2, height / 2, 'bg').setOrigin(0.5, 0.5).setVisible(true);
+
+        this.input.on('pointerdown', () => { this.startGame(); });
+        this.input.on('keydown', () => { this.startGame(); });
+    }
+
+    startGame() {
+        this.image.destroy();
+        this.game.sound.stopAll();
+        this.scene.stop();
+        this.scene.start('level1');
+    }
+}
