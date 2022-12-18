b2568edc6fa231055a0fd72730b7494306abc009
Moonshot <>
2022-12-18 16:45:58 +0100

    add wallet usage

--- src/scenes/Help.ts
+++ src/scenes/Help.ts
@@ -6,13 +6,9 @@ export default class Help extends Phaser.Scene {
     private line2!: Phaser.GameObjects.BitmapText;
     private line3!: Phaser.GameObjects.BitmapText;
     private line4!: Phaser.GameObjects.BitmapText;
-
+    
     constructor() {
         super('help')
-    }
-
-    init() {
-
     }
 
     preload() {
@@ -21,12 +17,51 @@ export default class Help extends Phaser.Scene {
 
     create() {
         const { width, height } = this.scale;
+        
+        this.add.image(width / 2, height / 2 - 150, 'logo').setDisplaySize(460, 196).setOrigin(0.5, 0.5);
 
-        this.add.image(width / 2, height / 2 - 128, 'logo').setDisplaySize(460, 196).setOrigin(0.5, 0.5);
+        this.createIntro();
 
-        this.line1 = this.add.bitmapText(width * 0.5, height / 2, 'press_start', 'Use CURSOR or WASD keys to move', 32)
+        this.input.on('pointerdown', () => { this.continueGame(); });
+        this.input.on('keydown', () => { this.continueGame(); });
+    }
+
+    private clearLines() {
+        this.line1.destroy();
+        this.line2.destroy();
+        this.line3.destroy();
+        this.line4.destroy();
+    }
+
+    private createIntro() {
+        const { width, height } = this.scale;
+
+        this.line1 = this.add.bitmapText(width * 0.5, height / 2, 'press_start', 'If you allow Hoppa to connect', 32)
             .setTint(0xffffff)
             .setOrigin(0.5);
+        this.line2 = this.add.bitmapText(width * 0.5, height / 2 + 48, 'press_start', 'Hoppa will check your Balance for', 32)
+            .setTint(0xffffff)
+            .setOrigin(0.5);
+        this.line3 = this.add.bitmapText(width * 0.5, height / 2 + 96, 'press_start', 'MSHOT and RA8BIT tokens', 32)
+            .setTint(0xffffff)
+            .setOrigin(0.5);
+        this.line4 = this.add.bitmapText(width * 0.5, height / 2 + 144, 'press_start', 'And NFTs from MoonBoxes.io', 32)
+            .setTint(0xffffff)
+            .setOrigin(0.5);
+                    
+        this.time.delayedCall(12000, () => {
+            this.clearLines();
+            this.createKeys();
+        });
+
+    }
+
+    private createKeys() {
+        const { width, height } = this.scale;
+
+        this.line1 = this.add.bitmapText(width * 0.5, height / 2, 'press_start', 'Use CURSOR or WASD keys to move', 32)
+        .setTint(0xffffff)
+        .setOrigin(0.5);
 
         this.line2 = this.add.bitmapText(width * 0.5, height / 2 + 48, 'press_start', 'Use SPACEBAR to jump', 32)
             .setTint(0xffffff)
@@ -40,8 +75,11 @@ export default class Help extends Phaser.Scene {
             .setTint(0xffffff)
             .setOrigin(0.5);
 
-        this.input.on('pointerdown', () => { this.continueGame(); });
-        this.input.on('keydown', () => { this.continueGame(); });
+            
+        this.time.delayedCall(6000, () => {
+            this.clearLines();
+            this.createIntro();
+        });
     }
 
     destroy() {
