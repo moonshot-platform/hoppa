db47510447f598970d67a4555e6274480eb08ecd
Moonshot <>
2022-12-18 16:47:00 +0100

    add destroy

--- src/scenes/Pause.ts
+++ src/scenes/Pause.ts
@@ -2,6 +2,9 @@ import Phaser from "phaser";
 import { sharedInstance as events } from "../scripts/EventManager";
 import * as SceneFactory from '../scripts/SceneFactory';
 export default class Pause extends Phaser.Scene {
+
+    private text!: Phaser.GameObjects.BitmapText;
+
     constructor() {
         super('pause')
     }
@@ -13,9 +16,13 @@ export default class Pause extends Phaser.Scene {
     create() {
         const { width, height } = this.scale;
 
-        this.add.bitmapText(width * 0.5, height * 0.5, 'press_start', 'GAME PAUSED', 64)
+        this.text = this.add.bitmapText(width * 0.5, height * 0.5, 'press_start', 'GAME PAUSED', 64)
             .setTint(0xff7300)
             .setOrigin(0.5);
 
+    }
+
+    destroy() {
+        this.text.destroy();
     }
 }
