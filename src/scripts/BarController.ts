3b94dcb3cab420305cb116028182c8ec4e3aef68
Moonshot <>
2022-12-18 16:28:16 +0100

    add solid as game object to be enabled when boss dies

---
+++ src/scripts/BarController.ts
@@ -0,0 +1,31 @@
+
+import { sharedInstance as events } from './EventManager';
+export default class BarController {
+    private scene: Phaser.Scene;
+    private sprite: Phaser.Physics.Matter.Sprite;
+    private name: string;
+  
+    constructor(
+        scene: Phaser.Scene,
+        sprite: Phaser.Physics.Matter.Sprite,
+        name: string
+    ) {
+        this.scene = scene;
+        this.sprite = sprite;     
+        this.name = name;
+
+        events.on( "wakeup-object", this.setInteractive, this);
+    }
+
+    private setInteractive() {
+        this.sprite.setCollidesWith([1,4]);
+    }
+
+    public getSprite() {
+        return this.sprite;
+    }
+
+    public destroy() {
+        this.sprite.destroy();
+    }
+}
