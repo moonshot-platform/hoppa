a900dca4944c4dfdb0ae6fea39eedcb2528b6ed4
Moonshot <>
2022-12-18 16:54:20 +0100

    add touched event, update enemy

--- src/scripts/PlantController.ts
+++ src/scripts/PlantController.ts
@@ -2,37 +2,40 @@ import StateMachine from "./StateMachine";
 import { sharedInstance as events } from './EventManager';
 
 export default class PlantController {
-    private scene: Phaser.Scene;
     private sprite: Phaser.Physics.Matter.Sprite;
     private stateMachine: StateMachine;
-
-    private moveTime = 0;
+    private touched: boolean;
+    private dead: boolean;
     private name: string;
-
+   
     constructor(
         scene: Phaser.Scene,
         sprite: Phaser.Physics.Matter.Sprite,
         name: string
     ) {
-        this.scene = scene;
         this.sprite = sprite;
         this.name = name;
+        this.touched = false;
+        this.dead = false;
         this.createAnims();
 
         this.stateMachine = new StateMachine(this);
 
         this.stateMachine.addState('idle', {
-            onEnter: this.idleOnEnter
+            onEnter: this.idleOnEnter,
         })
-            .addState('dead', {
-            })
-            .setState('idle');
+        .addState('dead', {
+            onEnter: this.deadOnEnter,
+        })
+        .setState('idle');
 
         events.on(this.name + '-stomped', this.handleStomped, this);
+        events.on(this.name + '-touched', this.handleTouched, this);
     }
 
     destroy() {
         events.off(this.name + '-stomped', this.handleStomped, this);
+        events.off(this.name + '-touched', this.handleTouched, this);
     }
 
     update(deltaTime: number) {
@@ -44,8 +47,22 @@ export default class PlantController {
     }
 
     private idleOnEnter() {
-        this.sprite.play('idle');
-        this.stateMachine.setState('move-left');
+        if(this.dead)
+            return;
+            
+        if(!this.touched) {
+            this.sprite.play('wait');
+        }
+        else {
+            this.sprite.play('idle');
+        }
+    }    
+
+    private deadOnEnter() {
+        this.sprite.play('dead');
+        this.sprite.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+            this.cleanup();
+        });
     }
 
     private handleStomped(plant: Phaser.Physics.Matter.Sprite) {
@@ -53,15 +70,35 @@ export default class PlantController {
             return;
         }
 
-        events.off(this.name + '-stomped', this.handleStomped, this);
+        if(!this.touched) {
+            this.touched = true;
+            this.sprite.play('grow');
+            this.sprite.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+                this.stateMachine.setState('dead');    
+            });
+        }
+        else {
+            events.off(this.name + '-stomped', this.handleStomped, this);
+            this.stateMachine.setState('dead');
+        }
 
-        this.sprite.play('dead');
-        this.sprite.on('animationcomplete', () => {
-            this.cleanup();
-        });
-
-        this.stateMachine.setState('dead');
+        this.dead = true;
     }
+
+    private handleTouched(plant: Phaser.Physics.Matter.Sprite) {
+        if (this.sprite !== plant) {
+            return;
+        }
+        if(this.touched == true || this.dead)
+            return;
+
+        this.touched = true;
+        events.off(this.name + '-touched', this.handleTouched, this);
+        this.sprite.play('grow').on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+            this.stateMachine.setState('idle');
+        });
+    }
+
 
     private cleanup() {
         this.sprite.destroy();
@@ -73,9 +110,31 @@ export default class PlantController {
             frameRate: 0.5,
             repeat: -1,
             frames: this.sprite.anims.generateFrameNames('plant', {
+                start: 4,
+                end: 5,
+                prefix: 'PlantTrap',
+                suffix: '.webp'
+            })
+        });
+        this.sprite.anims.create({
+            key: 'wait',
+            frameRate: 0.5,
+            repeat: -1,
+            frames: this.sprite.anims.generateFrameNames('plant', {
                 start: 1,
-                end: 2,
-                prefix: '0_Idle',
+                end: 1,
+                prefix: 'PlantTrap',
+                suffix: '.webp'
+            })
+        });
+        this.sprite.anims.create({
+            key: 'grow',
+            frameRate: 10,
+            repeat: 0,
+            frames: this.sprite.anims.generateFrameNames('plant', {
+                start: 2,
+                end: 4,
+                prefix: 'PlantTrap',
                 suffix: '.webp'
             })
         });
@@ -84,9 +143,9 @@ export default class PlantController {
             frameRate: 15,
             repeat: 0,
             frames: this.sprite.anims.generateFrameNames('plant', {
-                start: 1,
-                end: 3,
-                prefix: '1_Dead',
+                start: 6,
+                end: 8,
+                prefix: 'PlantTrap',
                 suffix: '.webp'
             })
         });
