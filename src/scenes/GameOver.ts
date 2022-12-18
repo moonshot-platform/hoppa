a9ecb2bbcff75df1a0cf88f1529d4249831173be
Moonshot <>
2022-12-18 16:45:39 +0100

    add text, add destroy

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -6,6 +6,7 @@ export default class GameOver extends Phaser.Scene {
     }
 
     private introMusic?: Phaser.Sound.BaseSound;
+    private text: Phaser.GameObjects.BitmapText;
 
     preload() {
         SceneFactory.preload(this);
@@ -13,10 +14,12 @@ export default class GameOver extends Phaser.Scene {
 
     create() {
         const { width, height } = this.scale;
+        
+        globalThis.musicTune = false;
 
         this.introMusic = SceneFactory.addSound(this, 'gameover', false);
 
-        this.add.bitmapText(width * 0.5, height * 0.5, 'press_start', 'GAME OVER', 64)
+        this.text = this.add.bitmapText(width * 0.5, height * 0.5, 'press_start', 'GAME OVER', 64)
             .setTint(0xff7300)
             .setOrigin(0.5);
 
@@ -24,10 +27,21 @@ export default class GameOver extends Phaser.Scene {
         this.input.on('keydown', () => { this.continueGame(); });
 
         this.cameras.main.shake(500);
+
+        this.time.delayedCall( 5000, () => {
+            this.add.bitmapText(width * 0.5, height * 0.5 + 96, 'press_start', 'PRESS ANY KEY', 24 )
+                .setTint(0xffffff)
+                .setOrigin(0.5);
+        });
     }
 
     continueGame() {
         this.introMusic?.stop();
         this.scene.start('start');
+    }
+
+    destroy() {
+        this.text.destroy();
+        this.introMusic?.destroy();
     }
 }
