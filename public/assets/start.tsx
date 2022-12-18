5957a71aad0c03ff52c6bd04ce7a3599d4dd91ff
Moonshot <>
2022-12-18 16:41:12 +0100

    update asset

--- public/assets/start.tsx
+++ public/assets/start.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="spritesheet_ground" tilewidth="64" tileheight="64" tilecount="64" columns="32">
- <image source="spritesheet_ground.webp" width="2048" height="128"/>
+ <image source="spritesheet_ground_extruded.webp" width="2048" height="128"/>
 </tileset>
