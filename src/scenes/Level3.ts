3cac492366fdd4b5561d642073347d386c5e440b
Moonshot <>
2022-12-25 16:34:50 +0100

    load asset

--- src/scenes/Level3.ts
+++ src/scenes/Level3.ts
@@ -212,7 +212,7 @@ export default class Level3 extends BaseScene {
         });
 
         this.matter.world.convertTilemapLayer(this.ground1, { label: 'ground', friction: 0, frictionStatic: 0 });
-        this.matter.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels, 1,true, true, false);
+        this.matter.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels, 1,true, true, false, false);
 
    /*     this.matter.world.drawDebug = false;
         this.input.keyboard.on("keydown-I", (event) => {
