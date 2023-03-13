68abed7547026903fceb0ab377df704fc49fa933
Moonshot <>
2023-03-13 22:48:38 +0100

    configuration updates

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	plugins: [],
 	base: '/hoppa/',
-	server: { host: '0.0.0.0', port: 8000 },
+	server: { host: '127.0.0.1', port: 8000 },
 	clearScreen: false,
 })
