70431e6f28fcb612aaddd30d0bffbed29ef3480c
Moonshot <>
2023-03-19 22:30:17 +0100

    multiple width by scale

--- src/scripts/AlignmentHelper.ts
+++ src/scripts/AlignmentHelper.ts
@@ -13,3 +13,19 @@ export function createAligned(scene, totalWidth, hei, texture, scrollFactor, sca
         x += m.width;
     }
 }
+
+export function createAligned2(scene, totalWidth, hei, texture, scrollFactor, scale) {
+    const w = scene.textures.get(texture).getSourceImage().width;
+    const count = Math.ceil(totalWidth / w) * scrollFactor;
+
+    let x = 0;
+    for (let i = 0; i < count; ++i) {
+        const m = scene.add
+            .image(x, hei, texture)
+            .setOrigin(0, 1)
+            .setScrollFactor(scrollFactor)
+            .setScale(scale);
+
+        x += (m.width * scale);
+    }
+}
